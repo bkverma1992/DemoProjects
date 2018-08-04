1304186f07a616456ef871a1508e06a35b6bb743
Chetan Rajauria <chetanrajauria@Chetans-MBP.lan>
2018-08-04 15:28:35 +0530

    Initial commit

---
+++ DemoProjects/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  DemoProjects
+//
+//  Created by Chetan Rajauria on 04/08/18.
+//  Copyright © 2018 Chetan Rajauria. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("demo check")
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
